1cb3b3d1009ed239a302eeee6aa7324369443d18
Adam Wenham <adamwenham64@gmail.com>
2021-08-22 21:37:31 +0200

    render enemy players in red

--- lib/adventure_time/game_display.ex
+++ lib/adventure_time/game_display.ex
@@ -1,39 +1,42 @@
 defmodule AdventureTime.GameDisplay do
   alias AdventureTime.Arena
 
-  def render_game(game) do
+  def render_game(game, player) do
     IO.write("\n")
-    render_arena(game.arena)
+    render_arena(game.arena, player)
     IO.write("\n")
     render_player_info(game.players)
     IO.write("\n")
   end
 
-  defp render_arena(arena) do
+  defp render_arena(arena, player) do
     arena
     |> Arena.grid_squares_as_flattened_list()
     |> Enum.chunk_every(10)
     |> Enum.each(fn arena_row ->
-      render_arena_row(arena_row)
+      render_arena_row(arena_row, player)
     end)
   end
 
-  defp render_arena_row(row) do
+  defp render_arena_row(row, player) do
     row
     |> Enum.each(fn grid_square ->
-      render_grid_square(grid_square)
+      render_grid_square(grid_square, player)
     end)
 
     IO.write("\n")
   end
 
-  defp render_grid_square(grid_square) do
+  defp render_grid_square(grid_square, player) do
     cond do
       grid_square.walkable == false ->
         IO.write("#{IO.ANSI.blue_background()}   #{IO.ANSI.reset()}")
 
-      length(Map.keys(grid_square.players)) > 0 ->
+      grid_square.players[player] == player ->
         IO.write("#{IO.ANSI.green_background()}   #{IO.ANSI.reset()}")
+
+      length(Map.keys(grid_square.players)) > 0 ->
+        IO.write("#{IO.ANSI.red_background()}   #{IO.ANSI.reset()}")
 
       true ->
         IO.write("#{IO.ANSI.white_background()}   #{IO.ANSI.reset()}")
